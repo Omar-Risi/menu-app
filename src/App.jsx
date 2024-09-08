c249372b58484f4209ef9a5ec9c27907b2b85548
Risi <Risi@users.noreply.github.com>
2024-09-08 12:29:55 +0400

    Items Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useRef } from "react";
+
 function App () {
 
   function CategoryItem({categorySource,categoryName}) {
@@ -11,18 +14,21 @@ function App () {
 
   function MenuItem({itemName, itemSource, itemPrice, children}) {
     return (
-      <button className="px-4 py-2 shadow-md rounded-md bg-white w-full grid grid-cols-2">
+      <button className="max-w-screen px-4 py-2 shadow-md rounded-md bg-white w-full grid grid-cols-2">
           <div>
-            <h2> {itemName} </h2>
-            <p> {children} </p>
-            <p> OMR{itemPrice}</p>
+            <h2 className="text-left text-xl font-bold font-mlpus w-fit"> {itemName} </h2>
+            <p className="text-left truncate text-gray-500 text-sm font-poppins font-medium"> {children} </p>
+            <p className="text-left text-orange-600 font-mplus font-extrabold mt-4 w-fit"> OMR{itemPrice}</p>
           </div>
           <div>
-            <img src={itemSource} alt="img" className="bg-gray-300 h-14 w-14 aspect-square rounded-md float-end" />
+            <img src={itemSource} alt="img" className="bg-gray-300 h-24 w-24 aspect-square rounded-md float-end" />
           </div>
       </button>
     )
   }
+
+  // component logic
+  const [categoryName, setCategoryName] = useState("Burgers");
 
   return (
     <main className="p-8">
@@ -34,19 +40,26 @@ function App () {
       <section className="mt-16">
         <h1 className="uppercase font-extrabold text-gray-900 text-2xl">Menu</h1>
 
-        <menu className="mt-4 flex-row flex gap-4 overflow-x-scroll">
+      <div className="bg-white p-4 mt-4 rounded-md shadow-md">
+        <menu className="mt-4 flex-row flex gap-4 overflow-x-scroll ">
           <CategoryItem categorySource={""} categoryName={"Burgers"}/>
           <CategoryItem categorySource={""} categoryName={"Fries"}/>
           <CategoryItem categorySource={""} categoryName={"Sides"}/>
           <CategoryItem categorySource={""} categoryName={"Juices"}/>
           <CategoryItem categorySource={""} categoryName={"Desserts"}/>
         </menu>
+      </div>
       </section>
 
       <section className="mt-8">
-        <menu>
-          <MenuItem itemName="Beef Burger" itemPrice="1"> Enjoy beef burger with...</MenuItem>
-        </menu>
+        <h2 className="uppercase text-left text-2xl font-mplus font-black"> {categoryName} </h2>
+        <menu className="flex-col flex gap-4 mt-8">
+          <MenuItem itemName="Item one"   itemPrice="1"> Item description</MenuItem>
+          <MenuItem itemName="Item two"   itemPrice="1"> Item description</MenuItem>
+          <MenuItem itemName="Item three" itemPrice="1"> Item description</MenuItem>
+          <MenuItem itemName="Item four"  itemPrice="1"> Item description</MenuItem>
+          <MenuItem itemName="Item five"  itemPrice="1"> Item description</MenuItem>
+        </menu> 
       </section>
       
     </main>
