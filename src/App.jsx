cae96b34f490f8eb457264d8723ff1274cde44e2
Risi <Risi@users.noreply.github.com>
2024-09-08 12:39:39 +0400

    Categories Switching Done

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App () {
 
   function CategoryItem({categorySource,categoryName}) {
     return (
-      <button>
+      <button className={(categoryName === currentCategoryName) ? "bg-orange-500/50 px-3 py-2 rounded-md text-orange-600 flex flex-col items-center" : " px-3 py-2 rounded-md text-gray-900 flex flex-col items-center" } onClick={() => {setCurrentCategoryName(categoryName)}}>
         <img src={categorySource} alt="image" className="h-16 w-16 aspect-square bg-gray-300 shadow-md rounded-md"/>
-        <p className="text-lg font-poppins font-semibold text-gray-900 mt-2"> {categoryName} </p>
+        <p className="text-lg font-poppins font-semibold mt-1"> {categoryName} </p>
       </button>
     )
   }
@@ -28,7 +28,8 @@ function App () {
   }
 
   // component logic
-  const [categoryName, setCategoryName] = useState("Burgers");
+  const [currentCategoryName, setCurrentCategoryName] = useState("Burgers");
+
 
   return (
     <main className="p-8">
@@ -42,17 +43,17 @@ function App () {
 
       <div className="bg-white p-4 mt-4 rounded-md shadow-md">
         <menu className="mt-4 flex-row flex gap-4 overflow-x-scroll ">
-          <CategoryItem categorySource={""} categoryName={"Burgers"}/>
-          <CategoryItem categorySource={""} categoryName={"Fries"}/>
-          <CategoryItem categorySource={""} categoryName={"Sides"}/>
+          <CategoryItem categorySource={""} categoryName={"Burgers"} />
+          <CategoryItem categorySource={""} categoryName={"Fries"} />
+          <CategoryItem categorySource={""} categoryName={"Sides"} />
           <CategoryItem categorySource={""} categoryName={"Juices"}/>
-          <CategoryItem categorySource={""} categoryName={"Desserts"}/>
+          <CategoryItem categorySource={""} categoryName={"Desserts"} />
         </menu>
       </div>
       </section>
 
       <section className="mt-8">
-        <h2 className="uppercase text-left text-2xl font-mplus font-black"> {categoryName} </h2>
+        <h2 className="uppercase text-left text-2xl font-mplus font-black"> {currentCategoryName} </h2>
         <menu className="flex-col flex gap-4 mt-8">
           <MenuItem itemName="Item one"   itemPrice="1"> Item description</MenuItem>
           <MenuItem itemName="Item two"   itemPrice="1"> Item description</MenuItem>
