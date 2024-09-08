c892233b272ae0096334586b17f848b216afa5c9
Risi <Risi@users.noreply.github.com>
2024-09-08 16:36:47 +0400

    Images Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
-import { useRef } from "react";
+
+import backIcon from "/Back.svg";
+import burger from "/burger.jpg";
+import coke from "/coke.jpg";
+import fries from "/fries.jpg";
+import sauce from "/sauce.jpg"
 
 function App () {
 
+  // component logic
+  const [currentCategoryName, setCurrentCategoryName] = useState("Burgers");
+  const [modalOpen, setModalOpen] = useState("0");
+  const [currentItem, setCurrentItem] = useState({name : "",source : ""})
+
+  function openModal(itemName, itemSource) {
+      setCurrentItem( {name : itemName, source: itemSource});
+      setModalOpen("screen");
+  }
+
   function Modal({itemName, itemSource}) {
     return (
-      <div className={`fixed left-0 bottom-0 bg-black/50 h-${modalOpen}  w-screen overflow-hidden ease-in-out duration-200 `}>
-        <button className="absolute top-0 left-0 m-4" onClick={() => {setModalOpen(false)}}> back </button>
+      <div className={`fixed left-0 bottom-0 bg-black/50 h-${modalOpen} w-screen overflow-hidden ease-in-out duration-200 `}>
+        <button className="absolute top-0 left-0 m-4" onClick={() => {setModalOpen(false)}}> 
+          <img src="/Back.svg" alt="back" className="text-white w-12 h-12 aspect-square" />
+        </button>
         <section className="absolute bottom-0 h-[80vh] bg-gray-100 w-screen p-8 flex flex-col items-start">
             <img src={itemSource} alt="Image" className="h-auto w-full lg:w-1/6 aspect-square bg-gray-400 rounded-md shadow-md"/>
 
@@ -27,16 +44,18 @@ function App () {
 
   function CategoryItem({categorySource,categoryName}) {
     return (
-      <button className={(categoryName === currentCategoryName) ? "bg-orange-500/50 px-3 py-2 rounded-md text-orange-600 flex flex-col items-center" : " px-3 py-2 rounded-md text-gray-900 flex flex-col items-center" } onClick={() => {setCurrentCategoryName(categoryName)}}>
-        <img src={categorySource} alt="image" className="h-16 w-16 aspect-square bg-gray-300 shadow-md rounded-md"/>
+      <button className={(categoryName === currentCategoryName) ? "bg-orange-500/50 px-3 py-2 rounded-md text-orange-600 flex flex-col items-center flex-1" : " px-3 py-2 rounded-md text-gray-900 flex flex-col items-center flex-1" } onClick={() => {setCurrentCategoryName(categoryName)}}>
+        <div className={`bg-[url('${categorySource}')] bg-cover bg-center w-16 h-16 aspect-square rounded-md shadow-md`}></div>
         <p className="text-lg font-poppins font-semibold mt-1"> {categoryName} </p>
       </button>
     )
   }
 
-  function MenuItem({itemName, itemSource, itemPrice, children}) {
+  function MenuItem({itemName, itemPrice, children}) {
+    const itemSource = "/burger.jpg";
+
     return (
-      <button className="max-w-screen px-4 py-2 shadow-md rounded-md bg-white w-full grid grid-cols-2" onClick={() => {openModal({itemName,itemSource})}}>
+      <button className="max-w-screen px-4 py-2 shadow-md rounded-md bg-white w-full grid grid-cols-2" onClick={() => { openModal(itemName, itemSource)}} >
           <div>
             <h2 className="text-left text-xl font-bold font-mlpus w-fit"> {itemName} </h2>
             <p className="text-left truncate text-gray-500 text-sm font-poppins font-medium"> {children} </p>
@@ -49,15 +68,6 @@ function App () {
     )
   }
 
-  // component logic
-  const [currentCategoryName, setCurrentCategoryName] = useState("Burgers");
-  const [modalOpen, setModalOpen] = useState("0");
-  const [currentItem, setCurrentItem] = useState({name : "",source : ""})
-
-  function openModal({itemName, itemSource}) {
-      setCurrentItem( {name : itemName, source: itemSource});
-      setModalOpen("screen");
-  }
 
   return (
     <main className="p-8">
@@ -70,11 +80,10 @@ function App () {
 
       <div className="bg-white p-4 mt-4 rounded-md shadow-md">
         <menu className="mt-4 flex-row flex gap-4 overflow-x-scroll ">
-          <CategoryItem categorySource={""} categoryName={"Burgers"} />
-          <CategoryItem categorySource={""} categoryName={"Fries"} />
-          <CategoryItem categorySource={""} categoryName={"Sides"} />
-          <CategoryItem categorySource={""} categoryName={"Juices"}/>
-          <CategoryItem categorySource={""} categoryName={"Desserts"} />
+          <CategoryItem categorySource={burger} categoryName={"Burgers"} />
+          <CategoryItem categorySource={fries} categoryName={"Fries"} />
+          <CategoryItem categorySource={sauce} categoryName={"Sides"} />
+          <CategoryItem categorySource={coke} categoryName={"Juices"}/>
         </menu>
       </div>
       </section>
